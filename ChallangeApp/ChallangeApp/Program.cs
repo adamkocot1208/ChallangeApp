b06bdbc9fd18b0d1b3a3bdf6b6c1b8db5ae35304
adamkocot1208 <72563552+adamkocot1208@users.noreply.github.com>
2023-12-11 10:44:21 +0100

    Zadanie dzien 4.

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Witaj w świecie programowania");
+﻿var name = "Kon";
+var sex = false;
+var age = 30;
+
+if (sex == true) //kobieta
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        if (age == 33 && name =="Ewa")
+        {
+            Console.WriteLine("Ewa, lat 33");
+        }
+        else
+        {
+            Console.WriteLine("Kobieta w wieku 30 lat lub więcej");
+        }
+    }
+
+}
+else //mezczyzna
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni Mężczyzna");
+            }
+}
